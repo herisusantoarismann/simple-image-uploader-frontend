c9605e340537848828634bfe93f4f873a449effe
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-04-16 18:20:36 +0700

    (+) integration with backend + download image

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,5 +1,5 @@
 <script></script>
 
-<button class="py-2 px-4 bg-[#3662E3] flex items-center gap-2 rounded-lg text-white">
+<button class="py-2 px-4 bg-[#3662E3] flex items-center gap-2 rounded-lg text-white" on:click>
     <slot />
 </button>
