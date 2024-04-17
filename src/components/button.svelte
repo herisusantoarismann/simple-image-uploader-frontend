bcf03df37dbd002fa4cd74d7faeb4c740dc5e4eb
heri.s.arisman@gmail.com <heri.s.arisman@gmail.com>
2024-04-17 12:21:00 +0700

    (+) reset image button

--- src/components/button.svelte
+++ src/components/button.svelte
@@ -1,5 +1,7 @@
-<script></script>
+<script>
+    export let variant = 'default'
+</script>
 
-<button class="py-2 px-4 bg-[#3662E3] flex items-center gap-2 rounded-lg text-white" on:click>
+<button class="py-2 px-4 bg-[#3662E3] flex items-center gap-2 rounded-lg text-white" class:bg-red-600={variant === 'danger'} on:click>
     <slot />
 </button>
